828f376bc80657954497d8bd2603ee6fdf75aae3
aashish50x <aashish50x@gmail.com>
2025-01-24 00:14:58 +0530

    adding experience column

--- index1.1.cpp
+++ index1.1.cpp
@@ -8,14 +8,16 @@ private:
     double salary;
     int age;
     string qualification;
+    int yearsOfWorking;
 
 public:
     // Method to set teacher details
-    void setDetails(string teacherName, double teacherSalary, int teacherAge, string teacherQualification) {
+    void setDetails(string teacherName, double teacherSalary, int teacherAge, string teacherQualification, int teacherYearsOfWorking) {
         name = teacherName;
         salary = teacherSalary;
         age = teacherAge;
         qualification = teacherQualification;
+        yearsOfWorking = teacherYearsOfWorking;
     }
 
     // Method to display teacher details
@@ -25,6 +27,7 @@ public:
         cout << "Salary: $" << salary << endl;
         cout << "Age: " << age << endl;
         cout << "Qualification: " << qualification << endl;
+        cout << "Years of Working: " << yearsOfWorking << endl;
     }
 };
 
@@ -35,6 +38,7 @@ int main() {
     double salary;
     int age;
     string qualification;
+    int yearsOfWorking;
 
     // Input teacher details
     cout << "Enter the name of the teacher: ";
@@ -46,9 +50,11 @@ int main() {
     cin.ignore(); // To consume the newline character left by cin
     cout << "Enter the qualification of the teacher: ";
     getline(cin, qualification);
+    cout << "Enter the years of working of the teacher: ";
+    cin >> yearsOfWorking;
 
     // Set and display teacher details
-    teacher1.setDetails(name, salary, age, qualification);
+    teacher1.setDetails(name, salary, age, qualification, yearsOfWorking);
     teacher1.displayDetails();
 
     return 0;
